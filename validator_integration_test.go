7df59e896dc619e48bdff6ad8bd9d00ec3357e27
Eugen Meltis <meltiseugen@gmail.com>
2019-09-12 14:17:16 +0300

    updated go.mod module name and added additional integration test.

--- validator_integration_test.go
+++ validator_integration_test.go
@@ -1,9 +1,11 @@
 package validator_test
 
 import (
+	"fmt"
+	"github.com/meltiseugen/validator"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"testing"
 	"time"
-	"validator"
 )
 
 func TestValidator_ValidateAndInit2(t *testing.T) {
@@ -57,6 +59,76 @@ func TestValidator_ValidateAndInit3(t *testing.T) {
 
 	time_, _ := time.Parse(time.RFC3339, "2019-08-21T09:00:00Z")
 	if !s.A.Equal(time_) || s.B != 1 || s.IS.C != 1 || s.IS.D != "qwerty" {
+		t.Error()
+	}
+}
+
+func TestValidator_ValidateAndInit5(t *testing.T) {
+	type StatsData struct {
+		MetricValue uint `bson:"metric_value" json:"metric_value"`
+		Entered     uint `bson:"entered" json:"entered"`
+		Exited      uint `bson:"exited" json:"exited"`
+		MaxCount    uint `bson:"max_count" json:"max_count"`
+	}
+
+	type Stats struct {
+		ID         primitive.ObjectID `bson:"-" json:"id" validate:"required,id" datakey:"a"`
+		Location   string             `bson:"-" json:"location" validate:"required" datakey:"b"`
+		Start      time.Time          `bson:"-" json:"start" validate:"required,time" datakey:"c"`
+		End        time.Time          `bson:"-" json:"end" validate:"required,time" datakey:"d"`
+		MetricType string             `bson:"metric_type" json:"metric_type" validate:"required,interval" datakey:"e"`
+		Data       []StatsData        `bson:"-" json:"data"`
+	}
+
+	v := validator.New()
+	_ = v.RegisterRule("id", func(mapKey string, m map[string]string, params ...string) error {
+		if mapValue, ok := m[mapKey]; ok {
+			_, err := primitive.ObjectIDFromHex(mapValue)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	_ = v.RegisterRule("interval", func(mapKey string, m map[string]string, params ...string) error {
+		if mapValue, ok := m[mapKey]; ok {
+			if mapValue != "minute" && mapValue != "hour" && mapValue != "day" &&
+				mapValue != "month" && mapValue != "year" {
+					return fmt.Errorf("map key '%s' with value '%s' does not match to " +
+						"any of (minute hour day month year)", mapKey, mapValue)
+			}
+		}
+		return nil
+	})
+
+	_ = v.RegisterConverter("primitive.ObjectID", func(value string, params ...string) (i interface{}, e error) {
+		oid, err := primitive.ObjectIDFromHex(value)
+		if err != nil {
+			return nil, err
+		}
+		return oid, nil
+	})
+
+	data := map[string]string{
+		"a": "000000000000000000000001",
+		"b": "42.23,54.56",
+		"c": "2019-08-21T09:00:00Z",
+		"d": "2019-08-24T09:00:00Z",
+		"e": "day",
+	}
+
+	s := Stats{}
+	err := v.ValidateAndInit(data, &s)
+	if err != nil {
+		t.Error()
+	}
+	if s.ID.String() != "ObjectID(\"000000000000000000000001\")" ||
+		s.Start.String() != "2019-08-21 09:00:00 +0000 UTC" ||
+		s.End.String() != "2019-08-24 09:00:00 +0000 UTC" ||
+		s.Location != "42.23,54.56" ||
+		s.MetricType != "day" {
+
 		t.Error()
 	}
 }
